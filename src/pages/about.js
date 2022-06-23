f643bc98ce11f7347a07bdb04d266027c10d0016
Nathan Batchelder <natebatchelder@gmail.com>
2022-06-23 16:42:46 -0700

    editing page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,8 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1>GeeksforGeeks is a Computer Science portal for geeks.</h1>
+      <h1>Vises are used to hold things.  Hold them hard.  Real hard.</h1>
+      <h3>Proprietor</h3>
     </div>
   );
 };
